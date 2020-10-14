397621b5144d01be6fc05bf13b48b78363990357
katplee <62573178+katplee@users.noreply.github.com>
2020-10-14 20:59:07 +0900

    Initial file backup
    Transferred all files from local storage to my repo.

---
+++ Jamming the Curve/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public bool anyKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(anyKey = Input.GetKeyDown("space")){
+            Debug.Log("A key or mouse click has been detected");
+        }
+    }
+}
